45d429c5a539f4f77a258ac3e6d2233069145621
Francesca Bueti <fbueti@gmail.com>
2018-02-05 15:27:18 -0500

    updated preview

--- js/playbook-nav.js
+++ js/playbook-nav.js
@@ -17,6 +17,26 @@ function currentSection(section, string) {
   }
 }
 
+function currentSectionMobile(section, string) {
+  if (touching(section, $("#mobile-playbook-nav"))){
+      $('#playbook-mobile-nav-top-text').text(string);
+  }
+}
+
+$("#playbook-mobile-nav-top").click(function(){
+  if($( "#playbook-mobile-nav-top i" ).hasClass( "fa-caret-up" )){
+    $('#playbook-mobile-nav-top i').addClass("fa-caret-down");
+    $('#playbook-mobile-nav-top i').removeClass("fa-caret-up");
+    $('#mobile-playbook-nav ul').show();
+  }
+  else{
+    $('#playbook-mobile-nav-top i').addClass("fa-caret-up");
+    $('#playbook-mobile-nav-top i').removeClass("fa-caret-down");    
+    $('#mobile-playbook-nav ul').hide();
+  }
+})
+
+
 function setNav(){
     currentSection($("#intro"), "#intro");
     currentSection($("#01"), "#01");
@@ -26,6 +46,14 @@ function setNav(){
     currentSection($("#05"), "#05");
     currentSection($("#06"), "#06");
     currentSection($("#section-07"), "#section-07");
+    currentSectionMobile($("#intro"), "Introduction");
+    currentSectionMobile($("#01"), "01 - Choose a Problem to Solve");
+    currentSectionMobile($("#02"), "02 - Form a Team");
+    currentSectionMobile($("#03"), "03 - Conduct User Research");
+    currentSectionMobile($("#04"), "04 - Explore Data");
+    currentSectionMobile($("#05"), "05 - Design & Build a Product");
+    currentSectionMobile($("#06"), "06 - Share with End Users & Stakeholders");
+    currentSectionMobile($("#section-07"), "07 - Keep Improving, Share Metrics & Feedback");
 }
 
 $(document).ready(function(){
