c51623d693f4f01ab4a1d2675412695bb456d017
em-fong <ef1389@nyu.edu>
2018-07-12 13:30:00 -0400

    summer 2018 working updates: small changes to homepage, new sprint page with js effects

--- CHANGES.md
+++ CHANGES.md
@@ -1,14 +1,30 @@
-Changes made as of 2/7/2018
+## Changes made as of 2/7/2018
 
-- All pages:
-	- Updated footer to collapse properly on mobile
+### All pages:
+* Updated footer to collapse properly on mobile
 
-- Home:
-	- Updated Cisco link
-	- Added mobile compatibility
+### Home:
+* Updated Cisco link
+* Added mobile compatibility
 
-- Projects:
-	- Added mobile compatibility
+### Projects:
+* Added mobile compatibility
 
-- Toolkit:
-	- Added mobile compatibility, including a collapsed mobile nav
+### Toolkit:
+* Added mobile compatibility, including a collapsed mobile nav
+
+## Changes made as of 6/19/2018
+
+### All:
+* Renamed "Get Started" to "Our Toolkit"
+
+### Home:
+* Renamed "In the Media" to "Media Archive"
+* Changed "Join the Next Cohort" email to a current one
+* Added sprint date
+
+### Projects:
+* Redesigned "Return to Top" button & added mobile compatibility
+
+### Files Added:
+* return-top-mobile.js
---
+++ js/grow-text.js
@@ -0,0 +1,88 @@
+function growText(block) {
+  if (block === "research") {
+    let element = document.getElementById("research-text");
+    element.style.webkitTransform = "scale(1.1,1.1)";
+
+    let text = document.getElementsByClassName("timeline-text-content");
+    text[0].style.color = "black";
+
+    let bar = document.getElementsByClassName("timeline-bar");
+    bar[0].style.backgroundColor = "#00a6d2";
+
+    let headers = document.getElementsByClassName("timeline-text-header");
+    headers[0].style.color = "#00A6D2";
+  }
+
+  if (block === "development") {
+    let element = document.getElementById("development-text");
+    element.style.webkitTransform = "scale(1.1,1.1)";
+
+    let text = document.getElementsByClassName("timeline-text-content");
+    text[1].style.color = "black";
+
+    let bar = document.getElementsByClassName("timeline-bar");
+    bar[1].style.backgroundColor = "#00a6d2";
+
+    let headers = document.getElementsByClassName("timeline-text-header");
+    headers[1].style.color = "#00A6D2";
+  }
+
+  if (block === "demo") {
+    let element = document.getElementById("demo-text");
+    element.style.webkitTransform = "scale(1.1,1.1)";
+
+    let text = document.getElementsByClassName("timeline-text-content");
+    text[2].style.color = "black";
+
+    let bar = document.getElementsByClassName("timeline-bar");
+    bar[2].style.backgroundColor = "#00a6d2";
+
+    let headers = document.getElementsByClassName("timeline-text-header");
+    headers[2].style.color = "#00A6D2";
+  }
+
+}
+
+function normalText(block) {
+  if (block === "research") {
+    let element = document.getElementById("research-text");
+    element.style.webkitTransform = "scale(1.0, 1.0)";
+
+    let text = document.getElementsByClassName("timeline-text-content");
+    text[0].style.color = "#aeb0b5";
+
+    let bar = document.getElementsByClassName("timeline-bar");
+    bar[0].style.backgroundColor = "#112e51";
+
+    let headers = document.getElementsByClassName("timeline-text-header");
+    headers[0].style.color = "#000";
+  }
+
+  if (block === "development") {
+    let element = document.getElementById("development-text");
+    element.style.webkitTransform = "scale(1.0, 1.0)";
+
+    let text = document.getElementsByClassName("timeline-text-content");
+    text[1].style.color = "#aeb0b5";
+
+    let bar = document.getElementsByClassName("timeline-bar");
+    bar[1].style.backgroundColor = "#112e51";
+
+    let headers = document.getElementsByClassName("timeline-text-header");
+    headers[1].style.color = "#000";
+  }
+
+  if (block === "demo") {
+    let element = document.getElementById("demo-text");
+    element.style.webkitTransform = "scale(1.0, 1.0)";
+
+    let text = document.getElementsByClassName("timeline-text-content");
+    text[2].style.color = "#aeb0b5";
+
+    let bar = document.getElementsByClassName("timeline-bar");
+    bar[2].style.backgroundColor = "#112e51";
+
+    let headers = document.getElementsByClassName("timeline-text-header");
+    headers[2].style.color = "#000";
+  }
+}
