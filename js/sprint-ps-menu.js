0194ea22a7f2edf2b1c719a378ea6df8c2385d20
Kiran Wattamwar <kiranwattamwar@Kirans-MBP.lan>
2019-06-27 15:50:21 -0400

    Adding 2019 Workforce Cohort problem statements, document and clean toggling script
    parent fba1a2b4141c13fd40c77b69f15f264fcd3dae93
    author Kiran Wattamwar <kiranwattamwar@Kirans-MBP.lan> 1561665141 -0400
    committer Kiran Wattamwar <kiranwattamwar@Kirans-MacBook-Pro.local> 1561741801 -0400
    
    Update changelog
    
    Update with 2019 problem statements, clean script for toggling year on Problem Statement page, and add document to problem statements
    
    Make calls to load external scripts over https instead of http, remove extraneous jquery import in sprints
    
    Updated the problem statements documents: links fixed, abridged version
    
    Update changelog
    
    Make calls to load external scripts over https instead of http, remove extraneous jquery import in sprints
    
    Updated the problem statements documents: links fixed, abridged version

--- js/sprint-ps-menu.js
+++ js/sprint-ps-menu.js
@@ -1,78 +1,8 @@
+// Update display to reflect the selected year
 function psToggle(problemSet) {
-  let problems2018 = document.getElementsByClassName('ps-2018');
-  let problems2017 = document.getElementsByClassName('ps-2017');
-  let problems2016 = document.getElementsByClassName('ps-2016');
+    $(".yr-btn").removeClass("yr-btn-active");
+    $(".ps").removeClass("ps-active");
 
-  let button2018 = document.getElementsByClassName('yr-btn')[0];
-  let button2017 = document.getElementsByClassName('yr-btn')[1];
-  let button2016 = document.getElementsByClassName('yr-btn')[2];
-
-  if (problemSet === "2018") {
-    for(var i = 0; i < problems2017.length; i++) {
-      problems2017[i].style.display = "none";
-    }
-
-    for(var i = 0; i < problems2016.length; i++) {
-      problems2016[i].style.display = "none";
-    }
-
-    for(var i = 0; i < problems2018.length; i++) {
-      problems2018[i].style.display = "inline";
-    }
-
-    button2018.style.color = "white";
-    button2018.style.backgroundColor = "#004469";
-
-    button2017.style.color = "";
-    button2017.style.backgroundColor = "";
-
-    button2016.style.color = "";
-    button2016.style.backgroundColor = "";
-
-
-  } else if (problemSet === "2017") {
-    for(var i = 0; i < problems2017.length; i++) {
-      problems2017[i].style.display = "inline";
-    }
-
-    for(var i = 0; i < problems2016.length; i++) {
-      problems2016[i].style.display = "none";
-    }
-
-    for(var i = 0; i < problems2018.length; i++) {
-      problems2018[i].style.display = "none";
-    }
-
-    button2018.style.color = "";
-    button2018.style.backgroundColor = "";
-
-    button2017.style.color = "white";
-    button2017.style.backgroundColor = "#004469";
-
-    button2016.style.color = "";
-    button2016.style.backgroundColor = "";
-
-  } else {
-    for(var i = 0; i < problems2017.length; i++) {
-      problems2017[i].style.display = "none";
-    }
-
-    for(var i = 0; i < problems2016.length; i++) {
-      problems2016[i].style.display = "inline";
-    }
-
-    for(var i = 0; i < problems2018.length; i++) {
-      problems2018[i].style.display = "none";
-    }
-
-    button2018.style.color = "";
-    button2018.style.backgroundColor = "";
-
-    button2017.style.color = "";
-    button2017.style.backgroundColor = "";
-
-    button2016.style.color = "white";
-    button2016.style.backgroundColor = "#004469";
-  }
-
+    $("#yr-btn-" + problemSet).addClass("yr-btn-active");
+    $(".ps-" + problemSet).addClass("ps-active");
 }
