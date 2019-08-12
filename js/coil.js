72ef4d7f0ff5be16715786138cca3f42d5f9942c
annaawang <anna.a.wang@gmail.com>
2019-08-12 16:36:35 -0500

    added color background changes

--- js/coil.js
+++ js/coil.js
@@ -1,14 +1,11 @@
 $(document).ready(function () {
     $(document).on("scroll", onScroll);
-    
  
     //smoothscroll
     $('a[href^="#"]').on('click', function (e) {
         e.preventDefault();
         $(document).off("scroll");
         
-        
-      
         var target = this.hash,
             menu = target;
         $target = $(target);
@@ -20,23 +17,62 @@ $(document).ready(function () {
         });
     });
 
-    var images = [
+    if($(window).width() < 481){
+         var images = [
+				  "img/coil-hero-mobile.png",
+				  "img/coil-hero-02.png",
+				]
+
+		var hero = document.getElementById('hero');
+		var i = 0;
+		setInterval(function() {
+	      hero.style.backgroundImage = "url(" + images[i] + ")";
+	      i = i + 1;
+	      if (i == images.length) {
+	        i =  0;
+		      }
+		}, 3000);
+   } 
+
+   else {
+   		var images = [
 				  "img/coil-hero-01.png",
 				  "img/coil-hero-02.png",
 				]
-	var hero = document.getElementById('hero');
-	var i = 0;
-	setInterval(function() {
-      hero.style.backgroundImage = "url(" + images[i] + ")";
-      i = i + 1;
-      if (i == images.length) {
-        i =  0;
-	      }
-	}, 3000);
+
+		var hero = document.getElementById('hero');
+		var i = 0;
+		setInterval(function() {
+	      hero.style.backgroundImage = "url(" + images[i] + ")";
+	      i = i + 1;
+	      if (i == images.length) {
+	        i =  0;
+		      }
+		}, 3000);
+   }
+
 });
+
+
 
 function onScroll(event){
     var scrollPos = $(document).scrollTop();
+    console.log(scrollPos);
+
+    if (scrollPos > 550) {
+         $('#mission').css('background', '#005FF3');
+      }
+  	if (scrollPos > 1400 || scrollPos <=550) {
+         $('#mission').css('background', 'white');
+      }
+
+     if (scrollPos >5000) {
+     	$('#contact').css('background', '#FF3D40');
+     }
+     if (scrollPos <= 5000) {
+     	$('#contact').css('background', 'white');
+     }
+
 
     if (scrollPos > 1440 && scrollPos < 5075) {
     	$('.coil-brand h1').css("color", "black");
@@ -45,9 +81,8 @@ function onScroll(event){
     	$('.coil-brand h1').css("color", "white");
       	$('.coil-nav ul li a').css("color", "white");
     }
-    $('a').each(function () {
+    $('a').not(document.getElementById('continue-a')).each(function () {
         var currLink = $(this);
-        //console.log(currLink);
         var refElement = $(currLink.attr("href"));
         if (refElement.position().top - 250 <= scrollPos && refElement.position().top + refElement.height() > scrollPos + 100) {
             $('#coil-nav ul li').removeClass("active-nav");
